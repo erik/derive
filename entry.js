805f7c7d56e4fadee90b578237edcfb9c016f598
Erik Price <github@erikprice.net>
2016-10-11 23:15:40 -0700

    webpack because im pretending i know what i am doing.

--- entry.js
+++ entry.js
@@ -1,3 +1,14 @@
+require('./style.css');
+
+var jquery = require('jquery');
+var L = require('leaflet');
+
+$(document).ready(() => {
+    var map = initMap();
+    initDropZone(map);
+});
+
+
 function initMap() {
     var map = L.map('background-map', {
         center: [34.0522, -118.243],
@@ -5,9 +16,9 @@ function initMap() {
     });
 
     L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', {
-	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-	subdomains: 'abcd',
-	maxZoom: 19
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+        subdomains: 'abcd',
+        maxZoom: 19
     }).addTo(map);
 
     return map;
@@ -19,10 +30,28 @@ function handleFileSelect(map, evt) {
     evt.stopPropagation();
     evt.preventDefault();
 
+    var polyLines = [];
+
     var files = evt.dataTransfer.files;
     for (var i = 0; i < files.length; ++i) {
         var reader = new FileReader();
-        reader.onload = (event) => addGPXFile(map, event);
+        reader.onload = (event) => {
+            readGPXFile(map, event, (line) => {
+                polyLines.push(line);
+
+                console.log('lines are', polyLines, files.length);
+
+                // If we've processed all the files.
+                if (polyLines.length === files.length) {
+                    console.log('here we go');
+                    polyLines.forEach(line => line.addTo(map));
+                    console.log('done');
+
+                    $('#modal').hide();
+                }
+            });
+
+        };
         reader.readAsText(files[i]);
     }
 }
@@ -35,7 +64,7 @@ function handleDragOver(evt) {
 }
 
 
-function addGPXFile(map, event) {
+function readGPXFile(map, event, callback) {
     var gpx = gpxParse.parseGpx(event.target.result, (error, data) => {
         if (error) {
             console.log('FAILED TO PARSE.', error);
@@ -63,7 +92,7 @@ function addGPXFile(map, event) {
 
         // TODO: on hover for name of thing
 
-        line.addTo(map);
+        callback(line);
     });
 }
 
@@ -71,4 +100,5 @@ function addGPXFile(map, event) {
 function initDropZone(map) {
     window.addEventListener('dragover', handleDragOver, false);
     window.addEventListener('drop', (e) => handleFileSelect(map, e), false);
+    window.addEventListener('dragstart', (_) => { console.log('modal show'); $('#modal').show()}, false);
 }
