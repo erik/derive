5152478f35eac55a4d4853219ef7d0b22af4d809
Erik Price <erikprice@openmail.co>
2016-10-26 10:02:33 -0700

    move code around

--- entry.js
+++ entry.js
@@ -25,44 +25,6 @@ function initMap() {
 }
 
 
-// Adapted from: http://www.html5rocks.com/en/tutorials/file/dndfiles/
-function handleFileSelect(map, evt) {
-    evt.stopPropagation();
-    evt.preventDefault();
-
-    var polyLines = [];
-
-    var files = evt.dataTransfer.files;
-    for (var i = 0; i < files.length; ++i) {
-        var reader = new FileReader();
-        reader.onload = (event) => {
-            readGPXFile(map, event, (line) => {
-                polyLines.push(line);
-
-                console.log('lines are', polyLines, files.length);
-
-                // If we've processed all the files.
-                if (polyLines.length === files.length) {
-                    console.log('here we go');
-                    polyLines.forEach(line => line.addTo(map));
-                    console.log('done');
-
-                    $('#modal').hide();
-                }
-            });
-
-        };
-        reader.readAsText(files[i]);
-    }
-}
-
-
-function handleDragOver(evt) {
-    evt.stopPropagation();
-    evt.preventDefault();
-    evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
-}
-
 
 function readGPXFile(map, event, callback) {
     var gpx = gpxParse.parseGpx(event.target.result, (error, data) => {
--- src/ui.js
+++ src/ui.js
@@ -1,6 +1,48 @@
 var jquery = require('jquery');
 var leaflet = require('leaflet');
 
+var map = require('./map');
+var gpx = require('./gpx');
+
+
+// Adapted from: http://www.html5rocks.com/en/tutorials/file/dndfiles/
+function handleFileSelect(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+
+    var polyLines = [];
+
+    var files = evt.dataTransfer.files;
+    for (var i = 0; i < files.length; ++i) {
+        var reader = new FileReader();
+        reader.onload = (event) => {
+            readGPXFile(map, event, (line) => {
+                polyLines.push(line);
+
+                console.log('lines are', polyLines, files.length);
+
+                // If we've processed all the files.
+                if (polyLines.length === files.length) {
+                    console.log('here we go');
+                    polyLines.forEach(line => line.addTo(map));
+                    console.log('done');
+
+                    $('#modal').hide();
+                }
+            });
+
+        };
+        reader.readAsText(files[i]);
+    }
+}
+
+
+function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+}
+
 
 function initialize() {
     window.addEventListener('dragover', (e) => {
