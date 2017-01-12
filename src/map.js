7e19cd3897d7d21586be7292def5573d955c3ac7
Erik Price <github@erikprice.net>
2017-01-12 14:28:57 -0800

    ASI, let, import

--- src/map.js
+++ src/map.js
@@ -1,9 +1,9 @@
-var leaflet = require('leaflet'),
-    _ = require('leaflet-providers');
+import leaflet from 'leaflet'
+import 'leaflet-providers'
 
 
 // Los Angeles is the center of the universe
-const INIT_COORDS = [34.0522, -118.243];
+const INIT_COORDS = [34.0522, -118.243]
 
 const DEFAULT_OPTIONS = {
     theme: 'CartoDB.DarkMatter',
@@ -13,41 +13,38 @@ const DEFAULT_OPTIONS = {
         opacity: 0.5,
         smoothFactor: 1
     }
-};
+}
 
 
-class GpxMap {
+export default class GpxMap {
     constructor(options) {
-        this.options = options || DEFAULT_OPTIONS;
+        this.options = options || DEFAULT_OPTIONS
 
         this.map = leaflet.map('background-map', {
             center: INIT_COORDS,
             zoom: 10
-        });
+        })
 
-        this.switchTheme(this.options.theme);
-        this.requestBrowserLocation();
+        this.switchTheme(this.options.theme)
+        this.requestBrowserLocation()
     }
 
     switchTheme(themeName) {
-        var tileLayer = leaflet.tileLayer.provider(themeName);
-        tileLayer.addTo(this.map);
+        let tileLayer = leaflet.tileLayer.provider(themeName)
+        tileLayer.addTo(this.map)
     }
 
     // Try to pull geo location from browser and center the map
     requestBrowserLocation() {
         navigator.geolocation.getCurrentPosition((pos) => {
-            this.map.panTo([pos.coords.latitude, pos.coords.longitude]);
-        });
+            this.map.panTo([pos.coords.latitude, pos.coords.longitude])
+        })
     }
 
     addTrack(track) {
-        console.log('adding track', track.name);
-        var line = leaflet.polyline(track.points, this.options.lineOptions);
-        line.addTo(this.map);
+        console.log('adding track', track.name)
+        var line = leaflet.polyline(track.points, this.options.lineOptions)
+        line.addTo(this.map)
     }
 
 }
-
-
-module.exports = GpxMap;
