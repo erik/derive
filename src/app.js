7e5ffcc60f9d440d53faeaff6b1e12f93fd7a042
Erik Price <github@erikprice.net>
2017-04-06 15:21:02 -0700

    Polyfill canvas.toBlob

--- src/app.js
+++ src/app.js
@@ -11,4 +11,27 @@ function app() {
     initialize(map)
 }
 
+
+// Safari (and probably some other older browsers) don't support canvas.toBlob,
+// so polyfill it in.
+//
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toBlob#Polyfill
+if (!HTMLCanvasElement.prototype.toBlob) {
+    Object.defineProperty(HTMLCanvasElement.prototype, 'toBlob', {
+        value: function (callback, type, quality) {
+
+            var binStr = atob(this.toDataURL(type, quality).split(',')[1]),
+                len = binStr.length,
+                arr = new Uint8Array(len);
+
+            for (var i = 0; i < len; i++ ) {
+                arr[i] = binStr.charCodeAt(i);
+            }
+
+            callback(new Blob([arr], {type: type || 'image/png'}));
+        }
+    });
+}
+
+
 jquery(document).ready(app)
