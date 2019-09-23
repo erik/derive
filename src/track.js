0b7295c480331e6a00ae4513860f3a770857a4c7
Hugo <hugovk@users.noreply.github.com>
2019-09-23 10:28:54 +0300

    Use reduce to remove points with missing coords

--- src/track.js
+++ src/track.js
@@ -24,13 +24,18 @@ function extractGPXTracks(gpx) {
         let name = trk.name && trk.name.length > 0 ? trk.name[0] : 'untitled';
 
         trk.trkseg.forEach(trkseg => {
-            let points = trkseg.trkpt.map(trkpt => ({
-                lat: parseFloat(trkpt.$.lat),
-                lng: parseFloat(trkpt.$.lon),
-                // These are available to us, but are currently unused
-                // elev: parseFloat(trkpt.ele) || 0,
-                // time: new Date(trkpt.time || '0')
-            }));
+            let points = trkseg.trkpt.reduce(function(result, trkpt) {
+                if (typeof trkpt.$ !== 'undefined') {
+                    result.push({
+                        lat: parseFloat(trkpt.$.lat),
+                        lng: parseFloat(trkpt.$.lon),
+                        // These are available to us, but are currently unused
+                        // elev: parseFloat(trkpt.ele) || 0,
+                        // time: new Date(trkpt.time || '0')
+                    });
+                }
+                return result;
+            }, []);
 
             parsedTracks.push({points, name});
         });
